f3fba00008cc79f6d286a2bd94b1ace902233edc
Maciek Lubocki <mail2maciek@gmail.com>
2020-04-30 18:39:59 +0200

    New variables to handle scissors/stone/paper

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+//let randomNumber = Math.floor(Math.random() * 10) + 10;
+
+
 function printMessage(msg) {
     let div = document.createElement('div');
     div.innerHTML = msg;
     document.getElementById('messages').appendChild(div);
+
 }
 
 function clearMessages() {
     document.getElementById('messages').innerHTML = '';
+
 }
 
-let computerMove = `kamień`;
-let playerMove = `papier`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier'
+} else { playerMove = 'nożyce' }
+
+
+printMessage('Zagrałem ' + randomNumber + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
