cace360fc75d920f33c6aa9e84aed0465d1ef8f5
Maciek Lubocki <mail2maciek@gmail.com>
2020-04-30 23:29:53 +0200

    Added if else and css, problem with scss/css conversion

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-//let randomNumber = Math.floor(Math.random() * 10) + 10;
-
-
 function printMessage(msg) {
     let div = document.createElement('div');
     div.innerHTML = msg;
@@ -24,7 +20,46 @@ if (playerInput == '1') {
     playerMove = 'kamień';
 } else if (playerInput == '2') {
     playerMove = 'papier'
-} else { playerMove = 'nożyce' }
+} else if (playerInput == '3') {
+    playerMove = 'nożyce'
+} else { playerMove = 'nieznany ruch' }
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+//let randomNumber = Math.floor(Math.random() * 10) + 10;
+
+computerMove = randomNumber
+
+if (computerMove == '1') {
+    computerMove = 'kamień';
+} else if (computerMove == '2') {
+    computerMove = 'papier'
+} else { computerMove = 'nożyce' }
 
 
-printMessage('Zagrałem ' + randomNumber + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    result = 'Ty wygrywasz!';
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+    result = 'No to mamy remis';
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    result = 'Yupi joł wygrałem, a Ty przegrałeś.';
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    result = 'W.Y.G.R.A.Ł.E.Ś';
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+    result = 'Remis. Zagrajmy jeszcze raz!.';
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+    result = 'O kto tutaj jest mistrzem? Wygrałem. Szach mat!';
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+    result = 'Ha! I am the Champion.';
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    result = 'Buuu to niesprawiedliwe. Ja chce wygrać!';
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+    result = 'Heh kto ten program stworzył. Znowu remis?';
+} else if (computerMove == 'papier' && playerMove == 'nieznany ruch') {
+    result = 'Wpisz Synu poprawną wartość.';
+} else if (computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
+    result = 'Coś Ci ręcę drżą. Wpisz poprawną wartość';
+} else if (computerMove == 'kamień' && playerMove == 'nieznany ruch') {
+    result = 'No co tam? Masz do wyboru 1 , 2 lub 3';
+}
+
+printMessage('Zagrałem ' + computerMove + ', a Ty zagrałeś ' + playerMove + '. ' + result);
